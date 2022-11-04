b1fc420276f89109d362a62b6862b1bf1bd235d0
Jacob Bolden <jlbolden.pro@gmail.com>
2022-11-04 00:21:48 -0500

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,20 @@ import "../styles/globals.scss";
 import Layout from "../layouts";
 import ErrorBoundary from "../components/ErrorBoundary";
 import { ThemeProvider } from "../context/useTheme";
+import { Analytics } from "@vercel/analytics/react";
 
 const PokeBinder = ({ Component, pageProps }) => {
  return (
-  <ErrorBoundary>
-   <ThemeProvider>
-    <Layout>
-     <Component {...pageProps} />
-    </Layout>
-   </ThemeProvider>
-  </ErrorBoundary>
+  <>
+   <ErrorBoundary>
+    <ThemeProvider>
+     <Layout>
+      <Component {...pageProps} />
+     </Layout>
+    </ThemeProvider>
+   </ErrorBoundary>
+   <Analytics />
+  </>
  );
 };
 
